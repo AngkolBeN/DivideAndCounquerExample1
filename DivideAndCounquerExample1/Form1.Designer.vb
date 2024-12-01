eea0dc0f1277a35e1e7ab23bec0abcc551f22c45
QCU <QCU@DESKTOP-52EHIRT>
2024-12-01 14:54:01 +0800

    Add project files.

---
+++ DivideAndCounquerExample1/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSort
+        '
+        Me.btnSort.BackColor = System.Drawing.Color.Tan
+        Me.btnSort.Location = New System.Drawing.Point(126, 280)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(113, 32)
+        Me.btnSort.TabIndex = 0
+        Me.btnSort.Text = "ENTER MO BEH"
+        Me.btnSort.UseVisualStyleBackColor = False
+        '
+        'txtInput
+        '
+        Me.txtInput.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.txtInput.Location = New System.Drawing.Point(92, 136)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(169, 28)
+        Me.txtInput.TabIndex = 1
+        '
+        'txtOutput
+        '
+        Me.txtOutput.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.txtOutput.Location = New System.Drawing.Point(92, 217)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(169, 28)
+        Me.txtOutput.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.Label1.Font = New System.Drawing.Font("Arial", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(114, 167)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "ENTER THE NUMBER "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.Label2.Font = New System.Drawing.Font("Arial", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(144, 248)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "RESULT"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(371, 324)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.btnSort)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSort As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ DivideAndCounquerExample1/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+
+
+    'Merge Sort Function - Divide And Conquer
+
+    Public Function MergeSort(arr As Integer()) As Integer()
+
+        If arr.Length <= 1 Then
+
+            Return arr
+
+        End If
+
+        'Divide step
+
+        Dim mid As Integer = arr.Length \ 2
+        Dim left As Integer() = MergeSort(arr.Take(mid).ToArray()) ' Recursively divide left
+        Dim right As Integer() = MergeSort(arr.Skip(mid).ToArray()) ' Recursively divide right
+
+        'Conquer step - merge the two sorted halves
+
+
+        Return Merge(left, right)
+
+    End Function
+
+    'Function to merge two sorted arrays
+    Private Function Merge(left As Integer(), right As Integer()) As Integer()
+
+        Dim result As New List(Of Integer)()
+        Dim i As Integer = 0
+        Dim j As Integer = 0
+
+        'Merge the two arrays
+
+        While i < left.Length AndAlso j < right.Length
+
+            If left(i) < right(j) Then
+
+                result.Add(left(i))
+                i += 1
+            Else
+                result.Add(right(j))
+                j += 1
+            End If
+
+        End While
+
+        'Add any remaining elements
+
+        If i < left.Length Then
+
+            result.AddRange(left.Skip(i))
+        End If
+        If j < right.Length Then
+            result.AddRange(right.Skip(j))
+        End If
+
+        Return result.ToArray()
+    End Function
+
+    'Event handler for the Sort button click
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+
+        'Get the input numbers
+        Dim input As String = txtInput.Text.Trim()
+            If String.IsNullOrEmpty(input) Then
+            MessageBox.Show("Please enter a list Of numbers separated by commas.")
+
+            Return
+
+        End If
+
+        'Convert input string into an array of integers
+
+        Dim numbers As Integer()
+            Try
+            numbers = input.Split(","c).Select(Function(n) Integer.Parse(n.Trim())).ToArray()
+        Catch ex As Exception
+            MessageBox.Show("Please enter valid numbers separated by commas")
+
+            Return
+
+        End Try
+
+        'Apply MergeSort using Divide And Conquer
+        Dim sortedNumbers As Integer() = MergeSort(numbers)
+
+        'Show the sorted numbers in the output box
+
+        txtOutput.Text = String.Join(",", sortedNumbers)
+
+
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
